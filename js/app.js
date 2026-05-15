a22ae33518873a93a7ff23366f2f38a2e7492e00
LitanMolla <litanmolla9@gmail.com>
2026-05-15 21:45:20 +0600

    added calling and cancel btn form hide

--- js/app.js
+++ js/app.js
@@ -10,6 +10,7 @@ const inputPhone = getElement('inputPhone')
 const saveBtn = getElement('saveBtn')
 const addContactBtn = getElement('addContactBtn')
 const addContactForm = getElement('addContactForm')
+const cancelBTN = getElement('cancelBTN')
 
 const contacts = [
     {
@@ -29,7 +30,7 @@ const showContactList = (list) => {
                 <p class="text-lg">${contact.number}</p>
                 <div class="grid grid-cols-3 gap-4">
                     <button
-                        class="bg-fuchsia-500 py-1 rounded-md cursor-pointer duration-300 hover:scale-105">Call</button>
+                        onclick='callFun(${contact.id})' class="bg-fuchsia-500 py-1 rounded-md cursor-pointer duration-300 hover:scale-105">Call</button>
                     <button
                         class="bg-blue-500 py-1 rounded-md cursor-pointer duration-300 hover:scale-105">Edit</button>
                     <button onclick='deleteContact(${contact.id})'
@@ -46,9 +47,12 @@ showContactList(contacts)
 const addContact = () => {
     const number = inputPhone.value;
     const name = inputName.value;
+    if (!name || !number) {
+        return alert("Please fillup all feild")
+    }
     const newContact = { id: contacts.length + 1, name, number }
-    inputName.value='';
-    inputPhone.value='';
+    inputName.value = '';
+    inputPhone.value = '';
     contacts.push(newContact)
     showContactList(contacts)
     addContactForm.classList.toggle('hidden')
@@ -57,13 +61,20 @@ const addContact = () => {
 
 saveBtn.addEventListener('click', addContact)
 const deleteContact = (id) => {
-    const index = contacts.findIndex(contact=>contact.id==id)
-    contacts.splice(index,1)
+    const index = contacts.findIndex(contact => contact.id == id)
+    contacts.splice(index, 1)
     showContactList(contacts)
 }
 
-const showForm = ()=> {
+const showForm = () => {
     addContactForm.classList.toggle('hidden')
     addContactBtn.classList.toggle('hidden')
 }
-addContactBtn.addEventListener('click',showForm)
+addContactBtn.addEventListener('click', showForm)
+cancelBTN.addEventListener('click', showForm)
+
+const callFun = (id) => {
+    const filter = contacts.filter((contact)=>contact.id==id)
+    const contact = filter[0]
+    return alert(`Caling ${contact.name} - ${contact.number}`)
+}
